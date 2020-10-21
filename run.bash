0e61a324c98d08ef5461e60ae8f0e6f367e6b9d8
Daniel de Andres <daniel.deandres@uam.es>
2020-10-22 00:49:24 +0200

    bugs at run.bash

--- run.bash
+++ run.bash
@@ -4,4 +4,4 @@ for i in 1 21 41 61 81 101 121 141 161 181 201 221 241 261 281 301
         do 
                 python3 generate-dm-maps.py $i $(( i + 20 )) > log$i-update 2> log$i-update &
         done
-python3 generate-sz-maps-G3X-progenitor.py 321 325 > log301-update 2> log301-update &
+python3 generate-dm-maps.py 321 325 > log301-update 2> log301-update &
